ee58f2d30b079638d4abf906a68d13fcf61207f9
Alyson Yan <alyson.cienciacomp@gmail.com>
2025-10-07 15:03:49 -0300

    Atualização completa: index, README, canvas, CSS, JS, favicon e organização de pastas

--- matrix.js
+++ matrix.js
@@ -1,54 +1,32 @@
 function efeitoMatrix(neo) {
-    // Variáveis globais
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    var largura = neo.width = window.innerWidth;
+    var altura = neo.height = window.innerHeight;
     var letras = Array(256).join(1).split('');
-    
-    // Ajuste para desenhar o efeito nas bordas (margens)
-    var desenhaMatrix = function () {
+
+    function desenhaMatrix() {
         var ctx = neo.getContext('2d');
-        
-        // Fundo Preto e transparência em .05
         ctx.fillStyle = 'rgba(0, 0, 0, 1)';
         ctx.fillRect(0, 0, largura, altura);
-        
-        // Letras verdes
         ctx.fillStyle = '#0F0';
 
-        // Definindo as posições para as bordas (margens)
-        letras.map(function (posicao_y, index) {
+        letras.map(function(pos_y, index) {
             var texto = String.fromCharCode(48 + Math.random() * 33);
-            
-            // Para a borda esquerda
-            var posicao_x = index * 10;
-            if (posicao_x < 100) {
-                ctx.fillText(texto, posicao_x, posicao_y);
+            var pos_x = index * 10;
+
+            if (pos_x < 100 || pos_x > largura - 100 || pos_y < 100 || pos_y > altura - 100) {
+                ctx.fillText(texto, pos_x, pos_y);
             }
 
-            // Para a borda direita
-            if (posicao_x > largura - 100) {
-                ctx.fillText(texto, posicao_x, posicao_y);
-            }
-
-            // Para as bordas superior e inferior
-            if (posicao_y < 100 || posicao_y > altura - 100) {
-                ctx.fillText(texto, posicao_x, posicao_y);
-            }
-
-            // Movimenta as letras para baixo, reinicia a coluna quando chega ao final
-            letras[index] = (posicao_y > altura + Math.random() * 1e4) ? 0 : posicao_y + 10;
+            letras[index] = (pos_y > altura + Math.random() * 1e4) ? 0 : pos_y + 10;
         });
-    };
+    }
 
-    // Inicia a animação do efeito
     setInterval(desenhaMatrix, 60);
 }
 
-// Função de carregar a página e passar o id do canvas para a função
 function carregar() {
     var canvas = document.getElementById('canvas');
     efeitoMatrix(canvas);
 }
 
-// Chama a função carregar quando a página estiver pronta
 window.onload = carregar;
